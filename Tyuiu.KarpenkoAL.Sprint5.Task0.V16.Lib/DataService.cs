9c019aa798a907704c8a22e4540ede1247813aab
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-10-07 05:52:37 +0500

    Add project files.

---
+++ Tyuiu.KarpenkoAL.Sprint5.Task0.V16.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KarpenkoAL.Sprint5.Task0.V16.Lib
+{
+    public class DataService : ISprint5Task0V16
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "OutPutFileTask0.txt");
+
+            double z = (2 * Math.Pow(x, 2) - 1) / (Math.Sqrt(Math.Pow(x, 2) - 2));
+
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, z.ToString());
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint5.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Tyuiu.KarpenkoAL.Sprint5.Task0.V16.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint5.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+
+            DataService ds = new DataService();
+            int testX = 3; 
+
+            string actualPath = ds.SaveToFileTextData(testX);
+
+            FileInfo fileInfo = new FileInfo(actualPath);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists, $"Файл не найден по пути: {actualPath}");
+        }
+    }
+}
