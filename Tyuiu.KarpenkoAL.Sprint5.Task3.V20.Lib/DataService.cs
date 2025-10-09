f3f12922eef81eaef2f10a4f66fd8e86c4070d90
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-10-09 22:10:49 +0500

    Task3

--- Tyuiu.KarpenkoAL.Sprint5.Task3.V20.Lib/DataService.cs
+++ Tyuiu.KarpenkoAL.Sprint5.Task3.V20.Lib/DataService.cs
@@ -13,18 +13,16 @@ namespace Tyuiu.KarpenkoAL.Sprint5.Task3.V20.Lib
             string tempPath = Path.GetTempPath();
             string path = Path.Combine(tempPath, "OutPutFileTask3.bin");
 
-            File.WriteAllText(path, string.Empty);
 
             double denominator = Math.Sqrt(Math.Pow(x, 2) + x);
 
             double y = Math.Round(x / denominator, 3);
 
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8)) 
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create))) 
             {
-                writer.Write(BitConverter.GetBytes(y));
+                writer.Write(y);
             }
 
-            File.WriteAllText(path, y.ToString());
             return path;
         }
     }
